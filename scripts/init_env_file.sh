6e494f56000b69527b5b5fef699f6e91cbacc74c
joel.clement <joelclems@gmail.com>
2023-09-13 23:34:32 +0200

    up

--- scripts/init_env_file.sh
+++ scripts/init_env_file.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # init_env_file.sh .env.example settings.ini .env
 
+set -ex
+
 template_file=$1
 settings_file=$2
 output_file=$3
@@ -13,6 +15,7 @@ while IFS= read -r line; do
     # skip comment
     [[ $line =~ ^#.* ]] && continue
     var_name=${line%%=*}
+    [[ -z "$var_name" ]] && continue
     sed -i "s${sep}${var_name}=.*${sep}${line}${sep}" $output_file
     grep "${var_name}=" ${output_file}
 done < ${settings_file}
